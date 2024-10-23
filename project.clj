09f309b6ae493898296315a5f81e365d9a1dcce3
Jeremiah Coyle <jcoyle@gmail.com>
2024-10-22 22:23:31 -0600

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.paintparty/fireworks-stubs "0.7.1"
+(defproject io.github.paintparty/fireworks-stubs "0.8.0"
   :description "Fireworks library for production builds"
   :url "https://github.com/paintparty/fireworks"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
